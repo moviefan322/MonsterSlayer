433bdc1ddaa54ef95956e9e05b62a5c9f86678b6
moviefan322 <philman202@yahoo.com>
2024-05-12 11:20:57 -0400

    adds battleLog

--- app.js
+++ app.js
@@ -8,7 +8,25 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
+      logMessages: [],
     };
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "draw";
+      } else if (value <= 0) {
+        this.winner = "player";
+      }
+    },
   },
   computed: {
     monsterBarStyles() {
@@ -28,20 +46,30 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.logMessages = [];
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getAttackValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('Player', 'attack', attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getAttackValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('Monster', 'attack', attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getAttackValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('Player', 'attack', attackValue);
       this.attackPlayer();
     },
     healPlayer() {
@@ -52,8 +80,19 @@ const app = Vue.createApp({
       } else {
         this.playerHealth += healValue;
       }
+      this.addLogMessage('Player', 'heal', healValue);
       this.attackPlayer();
     },
+    surrender() {
+      this.winner = "monster";
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
+    }
   },
 });
 
